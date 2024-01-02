5c63764ae37b574864ae37784d0aee7a67c07600
tom-wegrzyn <155281062+tom-wegrzyn@users.noreply.github.com>
2024-01-02 10:43:14 -0800

    Added a new solution for problem Two Sum
    Added two sum solution and test for it.

---
+++ Solutions/two_sum.py
@@ -0,0 +1,28 @@
+class Solution:
+    @staticmethod
+    def twoSum(nums: list[int], target: int) -> list[int]:
+        """
+        Takes in list of numbers and a target value and determines if two of the ints in the list can add
+        up to the target.
+
+        Parameters
+        ----------
+        nums : list[int]
+            The list of values that can potentially add up to the target.
+        target : int
+            The target sum.
+
+        Returns
+        ----------
+        list[int]
+            A list containing the indexes of the integers that add up to the target.
+        """
+        if len(nums) <= 1:
+            return []
+        for x in range(0, len(nums) - 1):
+            value = target - nums[x]
+            for c in range(x + 1, len(nums)):
+                if nums[c] == value:
+                    return [x, c]
+        return []
+
---
+++ Tests/test_two_sum.py
@@ -0,0 +1,20 @@
+import unittest
+
+from Solutions.two_sum import Solution
+
+class TestTwoSum(unittest.TestCase):
+    def setUp(self) -> None:
+        self.list1 = [2,7,11,15]
+        self.target1 = 9
+        self.list2 = [3, 2, 4]
+        self.target2 = 6
+        self.list3 = [3, 3]
+        self.target3 = 6
+
+    def test_two_sum(self):
+        self.assertEqual(Solution.twoSum(self.list1, self.target1), [0, 1])
+        self.assertEqual(Solution.twoSum(self.list2, self.target2), [1, 2])
+        self.assertEqual(Solution.twoSum(self.list3, self.target3), [0, 1])
+
+if __name__ == '__main__':
+    unittest.main()
